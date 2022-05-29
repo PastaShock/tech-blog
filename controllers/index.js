4141ca4e5bc4f2d65a416514819576278bb8c7c2
George Pastushok <gpastushok@yahoo.com>
2022-05-28 22:30:10 -0700

    added buttons to the homepage, fiddled with the routing, added dashboard routes, added create-post code in public, added home buttons functionality

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const dashRoutes = require('./dashboardRoutes')
 
 router.use('/', homeRoutes);
+router.use('/dash', dashRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
---
+++ public/js/addPost.js
@@ -0,0 +1,23 @@
+createPostHandler = (event) => {
+    event.preventDefault();
+    const title = document.querySelector('input[name="post-title"]').value
+    const body = document.querySelector('input[name="content"]').value;
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            body
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+          }
+    })
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('#create-post').addEventListener('submit', createPostHandler)
