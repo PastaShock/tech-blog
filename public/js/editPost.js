20bb61685c6f91b167a1976c41cb50a710931ef9
George Pastushok <gpastushok@yahoo.com>
2022-05-30 15:31:01 -0700

    completed delete

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,14 +1,17 @@
+const id = () => {
+    return window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+};
+
 async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="title"]').value.trim();
     const body = document.querySelector('input[name="body"]').value.trim();
 
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
 
-    const response = await fetch(`/api/posts/${id}`, {
+    const response = await fetch(`/api/posts/${id()}`, {
         method: 'PUT',
         body: JSON.stringify({ title, body }),
         headers: {
@@ -17,12 +20,33 @@ async function editFormHandler(event) {
     });
 
     if (response.ok) {
-        console.log(response);
-        // document.location.replace('/dash/');
+        document.location.replace('/dash/');
     } else {
         alert(response.statusText);
     }
 
 }
+
+async function deleteFormHandler(event) {
+    event.preventDefault();
+    const response = await fetch(`/api/posts/${id()}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+            postId: id()
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dash/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+
+document.querySelector('.deletePost').addEventListener('click', deleteFormHandler);
 
 document.querySelector('.editPost').addEventListener('submit', editFormHandler);
